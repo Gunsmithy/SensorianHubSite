674352ee2f9b6ad9152f5deb6c11175903d9728e
Dylan Kauling <gunsmithy@gmail.com>
2017-02-10 07:58:23 -0500

    Removed mysql module, changed networking
    Removed mysql PHP module since that's not a thing, mysqli is all we
    needed. Removed legacy container link and replaced with docker network.

--- php_site/public_html/dbconnect-docker.php
+++ php_site/public_html/dbconnect-docker.php
@@ -1,6 +1,6 @@
 <?php
 // Change credentials to that of your MySQL Database
-$servername = "172.17.0.1";
+$servername = "172.20.0.2";
 $username = "Sensorian";
 $password = "example_password";
 $dbname = "SensorianHubSiteDB";
--- php_site/site_proxy.sh
+++ php_site/site_proxy.sh
@@ -6,4 +6,4 @@ sed -e 's/"example_password"/"sensorian_user_password"/g' public_html/dbconnect-
 
 # Start nginx proxy and container for hosting with other containers
 sudo docker run -d -p 80:80 -v /var/run/docker.sock:/tmp/docker.sock:ro jwilder/nginx-proxy
-sudo docker run -d --link SensorianHub_Database:mysql -e VIRTUAL_HOST=sensorianhub.nextproject.ca --name SensorianHub_Site sensorianhub_site:latest
+sudo docker run -d -e VIRTUAL_HOST=sensorianhub.nextproject.ca --name SensorianHub_Site --network=SensorianNet --ip=172.20.0.1 sensorianhub_site:latest
