72b1efe99557d3909e4b595cff43489fbabeb072
Dylan Kauling <gunsmithy@gmail.com>
2017-02-10 11:57:14 -0500

    Named proxy
    Named proxy for ease of management like the rest.

--- php_site/site_proxy.sh
+++ php_site/site_proxy.sh
@@ -5,5 +5,5 @@ sed -e 's/"example_password"/"sensorian_user_password"/g' public_html/dbconnect-
 sudo docker build -t "sensorianhub_site" .
 
 # Start nginx proxy and container for hosting with other containers
-sudo docker run -d -p 80:80 --net=SensorianNet -v /var/run/docker.sock:/tmp/docker.sock:ro jwilder/nginx-proxy
+sudo docker run -d -p 80:80 --net=SensorianNet -v /var/run/docker.sock:/tmp/docker.sock:ro --name=Sensorian_Proxy jwilder/nginx-proxy
 sudo docker run -d -e VIRTUAL_HOST=sensorianhub.nextproject.ca --name SensorianHub_Site --network=SensorianNet --ip=172.20.0.20 sensorianhub_site:latest
