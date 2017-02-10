249016131504cac8a18847ac3cdb9f47d3077602
Dylan Kauling <gunsmithy@gmail.com>
2017-02-10 09:47:58 -0500

    Reverting import script
    Nevermind, unless chaining on the import properly, I think it's best to
    execute it separately afterwards.

--- docker_all_noproxy.sh
+++ docker_all_noproxy.sh
@@ -3,8 +3,6 @@ sudo docker network create --subnet=172.20.0.0/16 SensorianNet
 cd mysql_database
 chmod +x docker_mysql.sh
 ./docker_mysql.sh
-chmod +x import.sh
-./import.sh
 cd ..
 cd php_site
 chmod +x site_noproxy.sh
