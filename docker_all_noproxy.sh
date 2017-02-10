557c4a72fa80068c134a55a169d19ec5ec1852e3
Dylan Kauling <gunsmithy@gmail.com>
2017-02-10 09:40:05 -0500

    Added import back
    Seems like the issue may have been from the container crashing from not
    enough memory. Trying import in one go again.

--- docker_all_noproxy.sh
+++ docker_all_noproxy.sh
@@ -3,6 +3,8 @@ sudo docker network create --subnet=172.20.0.0/16 SensorianNet
 cd mysql_database
 chmod +x docker_mysql.sh
 ./docker_mysql.sh
+chmod +x import.sh
+./import.sh
 cd ..
 cd php_site
 chmod +x site_noproxy.sh
--- docker_all_proxy.sh
+++ docker_all_proxy.sh
@@ -1,7 +1,10 @@
+# git clone https://github.com/sensorian/SensorianHubSite.git --branch Dockerization --single-branch && cd SensorianHubSite && chmod +x docker_all_proxy.sh && ./docker_all_proxy.sh
 sudo docker network create --subnet=172.20.0.0/16 SensorianNet
 cd mysql_database
 chmod +x docker_mysql.sh
 ./docker_mysql.sh
+chmod +x import.sh
+./import.sh
 cd ..
 cd php_site
 chmod +x site_proxy.sh
