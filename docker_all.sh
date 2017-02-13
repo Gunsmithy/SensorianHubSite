2e604bd4a4ce2cfc05b4b0187416e1aee2d07715
Dylan Kauling <gunsmithy@gmail.com>
2017-02-13 15:18:38 -0500

    Changed commented script
    No locker on Dockerization branch, can clone from master.

--- docker_all.sh
+++ docker_all.sh
@@ -1,4 +1,4 @@
-# git clone https://github.com/sensorian/SensorianHubSite.git --branch Dockerization --single-branch && cd SensorianHubSite && chmod +x docker_all.sh && ./docker_all.sh
+# git clone https://github.com/sensorian/SensorianHubSite.git && cd SensorianHubSite && chmod +x docker_all.sh && ./docker_all.sh
 sudo docker network create --subnet=172.20.0.0/16 SensorianNet
 cd mysql_database
 chmod +x docker_mysql.sh
