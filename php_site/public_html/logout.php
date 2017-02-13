b99a0233d14040625369b106b1a419ac7575c8ba
Dylan Kauling <gunsmithy@gmail.com>
2017-02-13 15:09:01 -0500

    Renamed loginPage, fixed indentation bug
    Renamed loginPage.php to index.php so it should be the default page when
    going to the website URL. Removed indentation at the start of
    displayTable.php to hopefully fix an error about session already being
    started.

--- php_site/public_html/logout.php
+++ php_site/public_html/logout.php
@@ -4,7 +4,7 @@
 session_start();
 unset($_SESSION["userid"]);
 unset($_SESSION["name"]);
-$url = "loginPage.php";
+$url = "index.php";
 if(isset($_GET["session_expired"])) {
 	$url .= "?session_expired=" . $_GET["session_expired"];
 }
